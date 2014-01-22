878bb17448cbba9f573d586373028834b6c5f2f1
Ian Nastajus <ian.nastajus@gmail.com>
2014-01-22 12:32:24 -0500

    Dante teachings. This build errors on mousex, line.

--- main.py
+++ main.py
@@ -24,35 +24,20 @@ SCREEN_HEIGHT = 480
 
 
 class Bullet:
-    def __init__(self, bill_barrel, bill_base, boo):
-        self.bullet_image_original = pygame.image.load("bullet_bill.png")
-        self.bullet_image = pygame.image.load("bullet_bill.png")
+    def __init__(self, bill_barrel, boo):
+        self.bullet_original = pygame.image.load("bullet_bill.png")
+        self.bullet = pygame.image.load("bullet_bill.png")
         
-        self.size = self.bullet_image.get_size()
+        self.size = self.bullet.get_size()
         #self.position = [ SCREEN_WIDTH//2, SCREEN_HEIGHT - self.size[1] ]        
-        self.position =  ( bill_barrel.position[0] + bill_barrel.size[0]/2 - self.size[0]/2, bill_barrel.position[1] + bill_barrel.size[1]/2 - self.size[1]/2)  #(bill_barrel.position[0] + bill_barrel.size[0]/2, bill_barrel.position[1] + bill_barrel.size[1]/2)
-        #print bill_barrel.position,bill_barrel.size/2, self.size/2
+        self.position =  bill_barrel.position #(bill_barrel.position[0] + bill_barrel.size[0]/2, bill_barrel.position[1] + bill_barrel.size[1]/2)
 
         self.target = (boo.position[0] + boo.size[0]/2, boo.position[1] + boo.size[1]/2)
         self.direction = bill_barrel.angle
-
-        self.angle =  math.degrees(math.atan2(
-            bill_base.position[0] - boo.position[0] - boo.size[0]/2 - 32/2, 
-            bill_base.position[1] - boo.position[1] - boo.size[1]/2 - 28/2
-        ))
-
-        if self.angle < 0:
-            #self.more_rotation_amount = 90
-            print  self.angle
-            self.bullet_image = pygame.transform.flip(self.bullet_image, True, False)
-            self.bullet_image = pygame.transform.rotate(self.bullet_image, self.angle + 90)
-        else: 
-            self.bullet_image = pygame.transform.rotate(self.bullet_image, self.angle - 90)
-            #self.more_rotation_amount = 90
-        
+    
     
     def update( self ):
-        #global timeDelta
+        global timeDelta
         
         magnitude = 50.5
         
@@ -68,16 +53,11 @@ class Bullet:
             y = magnitude
             x = 0
             
-        self.position = ( self.position[0] - x, self.position[1] - y )
-        
-        #if self.angle < 0: self.bullet_image = pygame.transform.flip(self.bullet_image, True, False)
-
-        #
-        self.size = self.bullet_image.get_size()   #calculate new size after rotation
+        self.position = [ self.position[0] - x, self.position[1] - y ]
 
 
     def draw(self, target_surface):
-        target_surface.blit( self.bullet_image, self.position )
+        target_surface.blit( self.bullet, self.position )
         #print "drawing at " + str( self.position )
     
         blue = (0, 0, 255)
@@ -88,13 +68,18 @@ class Bullet:
         pygame.draw.line(screen, blue, point1, point2, width)
 
 
-        red = (255,0,0)
-        pygame.draw.line(target_surface, red, self.position, (self.position[0] + self.size[0], self.position[1]), 3)
-        pygame.draw.line(target_surface, red, self.position, (self.position[0], self.position[1] + self.size[1]), 3)
+class Blaster:
+    def __init__(self):
+        self.base = BillBase()
+        self.barrel = BillBarrel()
 
-        pygame.draw.line(target_surface, red, (self.position[0], self.position[1] + self.size[1]), (self.position[0] + self.size[0], self.position[1] + self.size[1] ), 3)
+    def update():
+        self.base.update()
+        self.barrel.update()
 
-        pygame.draw.line(target_surface, red, (self.position[0] + self.size[0], self.position[1]), (self.position[0] + self.size[0], self.position[1] + self.size[1] ), 3)
+    def draw():
+        self.base.draw()
+        self.barrel.draw()
 
 
 class BillBarrel:
@@ -115,14 +100,16 @@ class BillBarrel:
         #get top left posi of BillBase, draw it with it's magic number offset
 
 
-        self.angle = math.degrees(math.atan2(
-            bill_base.position[0] - boo.position[0] - boo.size[0]/2 - 32/2, 
-            bill_base.position[1] - boo.position[1] - boo.size[1]/2 - 28/2
-        ))
-        
+        self.angle = math.degrees(math.atan2(bill_base.position[0] - boo.position[0] - boo.size[0]/2, bill_base.position[1] - boo.position[1] - boo.size[1]/2))
+                
+        #self.rotated_side_length = (abs(math.cos(math.radians(self.angle))) + abs(math.sin(math.radians(self.angle))))*(self.size[0]) #thanks Dante
+
 
         self.bill_barrel_image = pygame.transform.rotate(self.bill_barrel_image_original, self.angle - 90)
-        self.size = self.bill_barrel_image.get_size() #calculate new size after rotation
+
+        #calculate new size
+        self.size = self.bill_barrel_image.get_size()
+        #print self.size 
 
         self.position = ( bill_base.position[0] + bill_base.point_of_rotation[0] - self.size[0]//2, bill_base.position[1] +  bill_base.point_of_rotation[1] - self.size[1]//2 )
         
@@ -216,7 +203,7 @@ class Boo:
         
         
     def draw(self, target_surface):
-        target_surface.blit( self.boo_image, (self.position[0] - self.size[0]/2, self.position[1] - self.size[1]/2) )
+        target_surface.blit( self.boo_image, self.position )
         #print self.position 
         
 
@@ -230,8 +217,6 @@ class Orbit:
 class Baby:
     def __init__(self, boo):
     
-        self.boo = boo;    
-
         self.baby_image = pygame.image.load("pics/Untitled" + str( random.randint(1,5) ) + ".png")
         self.size = self.baby_image.get_size()
         self.height = self.size[1]
@@ -239,74 +224,25 @@ class Baby:
         
         #x = 100 + random.randint(-100,100)
         #y = 100 + random.randint(-100,100)
-
         #print x, y
         #self.position = [x,y]
-        rangey = 200
-
-        x = boo.position[0] + random.randint(-rangey,rangey)
-        y = boo.position[1] + random.randint(-rangey,rangey)
-
+        
+        x = boo.position[0] + random.randint(-100,100)
+        y = boo.position[1] + random.randint(-100,100)
         #print x, y 
-        self.position = (x,y)
+        self.position = [x,y]
     
         
+        #self.position = [ 100 + random.randint(-100,100), 100 + random.randint(-100,100) ]
         
         #self.x = boo.position[0] 
         #self.y = boo.position[1]  
         #self.position = [ self.x , self.y ]
         #self.position = [100,100]  
-
-
-        #calculate distance from big boo
-        self.radius_dist = math.sqrt( math.pow(boo.position[0] - self.position[0], 2) + math.pow(boo.position[1] - self.position[1], 2) )
-
-        circum_dist = 2*math.pi*self.radius_dist
-
-
-        self.angle = math.atan2((boo.position[1] - self.position[1]), (boo.position[0] - self.position[0]))
-        #= math.radians(random.randint(0,360))
-        
-        self.magnitude = 1# / 1000.0
-
-        self.angle_velocity = self.magnitude/self.radius_dist   #rate that angle change 
-
-        print self.radius_dist, circum_dist, self.angle
-
-
-
-    def update(self):
-        global timeDelta
-
-        self.angle += self.angle_velocity 
-        
-
-        if (self.angle > math.pi):
-            self.angle = -math.pi + self.angle % math.pi
-        elif (self.angle < -math.pi):
-            self.angle = math.pi - self.angle % math.pi
-
-        print self.position
-
-        x = self.boo.position[0] + (self.radius_dist * math.cos(self.angle))# * (timeDelta / 1000)
-        y = self.boo.position[1] + (self.radius_dist * math.sin(self.angle))# * (timeDelta / 1000)
-
-
-        #x = (self.magnitude * math.sin(math.radians(self.angle))) * (timeDelta)
-        #y = (self.magnitude * math.cos(math.radians(self.angle))) * (timeDelta)
-
-        self.position = ( x, y )
-        #self.position = (  self.position[0] - x , self.position[1] - y )
-
-        #pick a direction --> clockwise
-        #what do i need
-        
-        #trajectory
-        #x = (magnitude * math.sin(math.radians(self.direction))) * (timeDelta / 1000.0)
-
+    
     
     def draw(self, target_surface):
-        target_surface.blit( self.baby_image, (self.position[0] - self.size[0]/2, self.position[1] - self.size[1]/2) )
+        target_surface.blit( self.baby_image, self.position )
     
         
         
@@ -323,19 +259,21 @@ class WorldThing:
         pygame.init()
         self.world_surface = pygame.display.set_mode( [SCREEN_WIDTH, SCREEN_HEIGHT] )
     
-        self.isPaused = False;
     
         self.boo = Boo()
-        self.bill_base = BillBase()
-        self.bill_barrel = BillBarrel()
+        self.blaster = Blaster()
+
+        mousex, mousey = 0,0
         
         
 
         self.babies = []
-        #redesign array to plot on a circle with equal distance with minor variationsf
-
-        for ii in range(0 , 15):
-            self.babies.append( Baby( self.boo ))
+        #redesign array to plot on a circle with equal distance with minor variations
+        self.babies.append( Baby( self.boo ))
+        self.babies.append( Baby( self.boo ))
+        self.babies.append( Baby( self.boo ))
+        self.babies.append( Baby( self.boo ))
+        self.babies.append( Baby( self.boo ))
         
         #self.babies = [Baby() for count in xrange(1)]
         #self.baby1 = Baby()
@@ -358,8 +296,6 @@ class WorldThing:
             prevTime = curTime
             curTime = pygame.time.get_ticks()
             timeDelta = curTime - prevTime
-
-            
             
             #print timeDelta
             
@@ -374,9 +310,6 @@ class WorldThing:
             
             if self.ev.type == pygame.QUIT:
                 break;
-
-            if (self.isPaused):
-                return
             
             if self.ev.type == pygame.KEYDOWN:
                 
@@ -388,36 +321,37 @@ class WorldThing:
             if self.ev.type == pygame.KEYUP:
                 self.key = self.ev.dict["key"]
                 keysDown[self.key] = False
-                            
+                
+            
             if(keysDown[K_a]):
                 self.boo.update( -1.0,0.0 ) 
-                self.bill_barrel.update( self.bill_base, self.boo )
+                #self.bill_barrel.update( self.bill_base, self.boo )
             elif(keysDown[K_d]):
                 self.boo.update( 1.0,0.0 )
-                self.bill_barrel.update( self.bill_base, self.boo )
-            else:
-               self.bill_barrel.update( self.bill_base, self.boo )
+                #self.bill_barrel.update( self.bill_base, self.boo )
+            #else:
+               #self.bill_barrel.update( self.bill_base, self.boo )
                
             if(keysDown[K_w]):
                 self.boo.update( 0.0,-1.0 )
-                self.bill_barrel.update( self.bill_base, self.boo )
+                #self.bill_barrel.update( self.bill_base, self.boo )
             elif(keysDown[K_s]):
                 self.boo.update( 0.0,1.0 )
-                self.bill_barrel.update( self.bill_base, self.boo )
-            else:
-                self.bill_barrel.update( self.bill_base, self.boo )
+                #self.bill_barrel.update( self.bill_base, self.boo )
+            #else:
+                #self.bill_barrel.update( self.bill_base, self.boo )
                 
-                
-            if(keysDown[K_p]):
-                self.isPaused = True
-            elif (keysDown[K_o]):
-                self.isPaused = False
+            #self.bill_barrel.update( self.bill_base, self.boo )
+
+            #self.blaster.update( self.boo, mousex, mousey )
+
                 
             if self.ev.type == pygame.MOUSEMOTION:
                 mousex, mousey = self.ev.pos
-                self.bill_base.update( mousex, mousey )
+                #self.bill_base.update( mousex, mousey )
+                #self.bill_barrel.update( self.bill_base, self.boo ) 
 
-                self.bill_barrel.update( self.bill_base, self.boo ) 
+                #self.blaster.update( self.boo, mousex, mousey )
         
                 
             if self.ev.type == pygame.MOUSEBUTTONDOWN:
@@ -427,7 +361,7 @@ class WorldThing:
                     
                     #first draw
                     
-                    self.bullets.append (  Bullet( self.bill_barrel, self.bill_base, self.boo ) )
+                    self.bullets.append (  Bullet( self.bill_barrel, self.boo ) )
 
                     self.bullet_exists = True
                     #for bullet in self.bullets:
@@ -440,15 +374,17 @@ class WorldThing:
                     bullet.update(  )
                     bullet.draw( self.world_surface )
 
-
-            for baby in self.babies:
-                baby.update()
-
             
+            
+            self.blaster.update( self.boo, mousex, mousey )
+
+
+
             # draws.
             self.boo.draw( self.world_surface ) 
-            self.bill_base.draw ( self.world_surface )
-            self.bill_barrel.draw ( self.world_surface )
+            #self.bill_base.draw ( self.world_surface )
+            #self.bill_barrel.draw ( self.world_surface )
+            self.blaster.draw( self.world_surface )
 
             
             for baby in self.babies:
